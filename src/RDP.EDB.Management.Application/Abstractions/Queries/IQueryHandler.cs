c5ade7dde0d5bb78fdc4e5825090971e81e48950
diedona <diedona@gmail.com>
2024-03-18 09:21:17 -0300

    sugestão do GPT/gemini

--- src/RDP.EDB.Management.Application/Abstractions/Queries/IQueryHandler.cs
+++ src/RDP.EDB.Management.Application/Abstractions/Queries/IQueryHandler.cs
@@ -2,5 +2,5 @@
 
 namespace RDP.EDB.Management.Application.Abstractions.Queries;
 
-public interface IQueryHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+public interface IQueryHandler<in TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
     where TRequest : IRequest<TResponse>;
