57aac87115e27a03deb40cafcbf6eba60e211a37
diedona <diedona@gmail.com>
2024-03-23 16:40:52 -0300

    test select

--- src/RDP.EDB.Management.WebApi/Endpoints/People/Get/GetPeopleEndpoint.cs
+++ src/RDP.EDB.Management.WebApi/Endpoints/People/Get/GetPeopleEndpoint.cs
@@ -2,8 +2,10 @@
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using RDP.EDB.Management.Application.Identification;
 using RDP.EDB.Management.Application.UseCases.People.Queries.Get;
 using RDP.EDB.Management.Infra.Contexts;
+using System.Linq;
 
 namespace RDP.EDB.Management.WebApi.Endpoints.People.Get;
 
@@ -16,6 +18,7 @@ public class GetPeopleEndpoint
         CancellationToken token
     )
     {
+        var users = context.Set<EbdUser>().Select(x => x).ToList();
         var result = await sender.Send(new GetPeopleQuery(), token);
         var people = mapper.Map<IEnumerable<GetPeopleResponse>>(result?.Data);
 
